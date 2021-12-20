4e2da64281be281f4b3a71b93d54a6106f4e4683
gianchiesa <chiesamghz@gmail.com>
2021-12-20 14:17:51 +0700

    add logic for rating and add splash screen

--- app/src/main/java/com/gianchiesamaghriza/nekosekai/DetailActivity.kt
+++ app/src/main/java/com/gianchiesamaghriza/nekosekai/DetailActivity.kt
@@ -2,9 +2,9 @@ package com.gianchiesamaghriza.nekosekai
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.RatingBar
-import android.widget.TextView
+import android.view.View
+import android.widget.*
+import androidx.core.view.isGone
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_detail.*
 import org.json.JSONArray
@@ -15,7 +15,16 @@ import java.io.Serializable
 class DetailActivity : AppCompatActivity() {
     private lateinit var breeds: TextView
     private lateinit var pic: ImageView
-    private lateinit var rate: RatingBar
+    private lateinit var intelRate: RatingBar
+    private lateinit var affectRate: RatingBar
+    private lateinit var energyRate: RatingBar
+    private lateinit var healthRate: RatingBar
+    private lateinit var rareRate: RatingBar
+    private lateinit var adaptRate: RatingBar
+    private lateinit var childRate: RatingBar
+    private lateinit var catRate: RatingBar
+    private lateinit var dogRate: RatingBar
+    private lateinit var strangerRate: RatingBar
     private lateinit var desc: TextView
     private lateinit var temp: TextView
     private lateinit var origin: TextView
@@ -35,11 +44,36 @@ class DetailActivity : AppCompatActivity() {
         val originText: String = intent.getSerializableExtra("objectorigin") as String
         val descText: String = intent.getSerializableExtra("objectdesc") as String
         val tempText: String = intent.getSerializableExtra("objecttemp") as String
-        val rate: String = intent.getSerializableExtra("objectintel") as String
-        val rateln = rate.length
-        if (rateln == 0)
-            ratingEnergy.numStars = 2
-        else ratingEnergy.rating = rate.toFloat()
+        val intelRate: String = intent.getSerializableExtra("objectintel") as String
+        val affectRate: String = intent.getSerializableExtra("objectaffect") as String
+        val energyRate: String = intent.getSerializableExtra("objectenergy") as String
+        val healthRate: String = intent.getSerializableExtra("objecthealth") as String
+        val rareRate: String = intent.getSerializableExtra("objectrare") as String
+        val adaptRate: String = intent.getSerializableExtra("objectadapt") as String
+        val dogRate: String = intent.getSerializableExtra("objectdogf") as String
+        val catRate: String = intent.getSerializableExtra("objectcatf") as String
+        val childRate: String = intent.getSerializableExtra("objectchildf") as String
+        val strangerRate: String = intent.getSerializableExtra("objectstrangerf") as String
+        val rateln = intelRate.length
+        if (rateln == 0) {
+            val linearLayout = findViewById(R.id.ratingAll) as LinearLayout
+            linearLayout.visibility = View.GONE
+            val org = findViewById(R.id.origin) as TextView
+            org.visibility = View.GONE
+        } else {
+            val btn = findViewById(R.id.searchBtn) as Button
+            btn.visibility = View.GONE
+            ratingIntel.rating = intelRate.toFloat()
+            ratingAdapt.rating = adaptRate.toFloat()
+            ratingAffect.rating = affectRate.toFloat()
+            ratingEnergy.rating = energyRate.toFloat()
+            ratingHealth.rating = healthRate.toFloat()
+            ratingRare.rating = rareRate.toFloat()
+            ratingCat.rating = catRate.toFloat()
+            ratingDog.rating = dogRate.toFloat()
+            ratingStranger.rating = strangerRate.toFloat()
+            ratingChild.rating = childRate.toFloat()
+        }
         breeds.text = name
         desc.text = descText
         Picasso.get().load(url).into(pic)
@@ -48,12 +82,21 @@ class DetailActivity : AppCompatActivity() {
 
 
         println("kental")
-        println(rate)
+        println(intelRate)
     }
     private fun initView() {
         breeds = findViewById(R.id.breeds) as TextView
         pic = findViewById(R.id.imgtest) as ImageView
-        rate = findViewById(R.id.ratingEnergy) as RatingBar
+        intelRate = findViewById(R.id.ratingIntel) as RatingBar
+        affectRate = findViewById(R.id.ratingAffect) as RatingBar
+        energyRate = findViewById(R.id.ratingEnergy) as RatingBar
+        healthRate = findViewById(R.id.ratingHealth) as RatingBar
+        rareRate = findViewById(R.id.ratingRare) as RatingBar
+        adaptRate = findViewById(R.id.ratingAdapt) as RatingBar
+        catRate = findViewById(R.id.ratingCat) as RatingBar
+        childRate = findViewById(R.id.ratingChild) as RatingBar
+        dogRate = findViewById(R.id.ratingDog) as RatingBar
+        strangerRate = findViewById(R.id.ratingStranger) as RatingBar
         desc = findViewById(R.id.descDetail) as TextView
         temp = findViewById(R.id.temp) as TextView
         origin = findViewById(R.id.origin) as TextView
